f1abc62dafca8085eaf7647801bfdf3323a79d3c
KEVEC <christophe.kevers@luminus.be>
2021-08-24 11:01:39 +0200

    Everything fine with CRUD

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ db = SQLAlchemy(app)
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    author=db.Column(db.String(20), nullable=False, default='N/A')
+    title = db.Column(db.String(250), nullable=False)
+    content = db.Column(db.Text(250), nullable=False)
+    author=db.Column(db.String(250), nullable=False, default='N/A')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
