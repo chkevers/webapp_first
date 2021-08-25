65b823860b50c7046410aac624e3947347e50372
KEVEC <christophe.kevers@luminus.be>
2021-08-25 06:37:02 +0200

    Startig the day

--- app.py
+++ app.py
@@ -72,6 +72,19 @@ def edit(id):
         return redirect('/posts')
     else:
         return render_template('edit.html', post=post)
+
+@app.route('/posts/new', methods=['GET','POST'])
+def new_post():
+    if request.method=='POST':
+        post_title=request.form['title']
+        post_author=request.form['author']
+        post_content=request.form['content']
+        new_post=BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
     
 if __name__ == "__main__":
     app.run(debug=True)
